f418906d387212c5e567bc2cc169be501b4bf27f
Alexcalyx <a.mason@pvp.co.jp>
2023-10-18 16:51:52 +0900

    Modularised Login Page

---
+++ lib/screens/login/loginUI.dart
@@ -0,0 +1,119 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:isms/screens/homePage.dart';
+import 'package:isms/userManagement/customUserProvider.dart';
+import 'package:isms/utitlityFunctions/auth_service.dart';
+import 'package:provider/provider.dart';
+
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  LoginPage({super.key});
+
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  Future<User?>? _signInFuture;
+  bool hasCheckedForChangedDependencies = false;
+
+  @override
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
+    if (!hasCheckedForChangedDependencies &&
+        FirebaseAuth.instance.currentUser != null) {
+      hasCheckedForChangedDependencies = true;
+      if (mounted) {
+        await AuthService().handleSignInDependencies(
+          context: context,
+          customUserProvider:
+              Provider.of<CustomUserProvider>(context, listen: false),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    CustomUserProvider customUserProvider =
+        Provider.of<CustomUserProvider>(context, listen: false);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            loginText(),
+            const SizedBox(height: 150),
+            bugHubLogo(),
+            const SizedBox(height: 150),
+            bugHubText(),
+            const SizedBox(height: 40),
+            signInButton(customUserProvider: customUserProvider),
+            SignInFutureBuilder(_signInFuture),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget loginText() {
+    return const Text('Login');
+  }
+
+  Widget bugHubLogo() {
+    return const SizedBox(
+      width: 150,
+      height: 150,
+      child: Text('LOGO'),
+    );
+  }
+
+  Widget bugHubText() {
+    return const Text('ISMS');
+  }
+
+  Widget signInButton({required CustomUserProvider customUserProvider}) {
+    if (_signInFuture == null) {
+      return ElevatedButton.icon(
+        onPressed: () {
+          setState(() {
+            _signInFuture = AuthService().signInWithGoogle(customUserProvider);
+          });
+        },
+        icon: const Icon(Icons.mail),
+        label: const Text('Sign in with Google'),
+      );
+    }
+    return const SizedBox.shrink();
+  }
+
+  Widget SignInFutureBuilder(Future<User?>? signInFuture) {
+    if (signInFuture != null) {
+      return FutureBuilder<User?>(
+        future: signInFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.data != null) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            });
+          }
+          return const CircularProgressIndicator();
+        },
+      );
+    }
+    return const SizedBox.shrink();
+  }
+}
--- lib/userManagement/createUser.dart
+++ lib/userManagement/createUser.dart
@@ -1,12 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:isms/models/customUser.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:isms/databaseOperations/databaseManager.dart';
+import 'package:isms/models/customUser.dart';
 
 class CreateUserDataOperations extends DatabaseManager {
   Future<void> createUser(String uid, CustomUser user) async {
     Map<String, dynamic> userJson = user.toMap();
     await db.collection('users').doc(uid).set(userJson);
+  }
+
+  Future<void> checkAndCreateUserIfNotExists(User? user) async {
+    if (user == null) return;
+
+    final userDoc = await db.collection('users').doc(user.uid).get();
+
+    if (!userDoc.exists) {
+      await createUser(
+          user.uid,
+          CustomUser(
+            username: user.displayName!,
+            email: user.email!,
+            role: 'user',
+            courses_started: [],
+            courses_completed: [],
+          ));
+    }
   }
 
   Future<QuerySnapshot<Map<String, dynamic>>> fetchAllUsers() {
