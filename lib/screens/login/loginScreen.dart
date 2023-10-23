73e75f4fdab8c9b821bccd7abe4add54ee0da3df
Rajarshi Mazumder <raj1156mazumder@gmail.com>
2023-10-23 18:05:46 +0900

    Fixed a couple of bugs in download functionality

--- lib/screens/login/loginScreen.dart
+++ lib/screens/login/loginScreen.dart
@@ -1,8 +1,3 @@
-import 'dart:convert';
-// import 'dart:html' as html;
-import 'dart:typed_data';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +9,6 @@ import 'package:isms/userManagement/loggedInUserProvider.dart';
 import 'package:isms/userManagement/userDataGetterMaster.dart';
 import 'package:isms/utitlityFunctions/auth_service.dart';
 import 'package:provider/provider.dart';
-import "package:universal_html/html.dart" as html;
 
 class LoginPage extends StatefulWidget {
   LoginPage({super.key});
@@ -60,47 +54,6 @@ class LoginPageState extends State<LoginPage> {
     print(userCredential.user?.displayName);
   }
 
-  Future<void> downloadCSV() async {
-    QuerySnapshot querySnapshot =
-        await FirebaseFirestore.instance.collection('users').get();
-    final List<QueryDocumentSnapshot> allData = querySnapshot.docs;
-
-    // Initialize a list to store the CSV data
-    List<List<dynamic>> csvData = [
-      // Define the headers
-      ['username', 'uid', 'email', 'courses_started']
-    ];
-    print(allData);
-    // Loop through the documents to populate the CSV data
-    for (QueryDocumentSnapshot snapshot in allData) {
-      Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
-      csvData.add([
-        data['username'].toString(),
-        data['uid'].toString(),
-        data['email'].toString(),
-        data['courses_started'].toString()
-      ]);
-    }
-    print(csvData);
-    // Create the CSV content
-    StringBuffer buffer = StringBuffer();
-    csvData.forEach((row) {
-      buffer.writeAll(row, ',');
-      buffer.write('\n');
-    });
-    print(buffer);
-    // Convert to Uint8List
-    Uint8List bytes = Uint8List.fromList(utf8.encode(buffer.toString()));
-
-    // Create Blob and download the file
-    final blob = html.Blob([bytes]);
-    final url = html.Url.createObjectUrlFromBlob(blob);
-    final anchor = html.AnchorElement(href: url)
-      ..setAttribute('download', 'data.csv')
-      ..click();
-    html.Url.revokeObjectUrl(url);
-  }
-
   @override
   Widget build(BuildContext context) {
     LoggedInUserProvider customUserProvider =
@@ -120,14 +73,7 @@ class LoginPageState extends State<LoginPage> {
                 onPressed: () {
                   GoogleSignInWeb();
                 },
-                child: Text('Google Login Web')),
-            ElevatedButton(
-                onPressed: () {
-                  // DataExporter dataExporter = DataExporter();
-                  // dataExporter.createCSV();
-                  downloadCSV();
-                },
-                child: Text('Download CSV')),
+                child: Text('Google Test Login Web')),
             signInButton(customUserProvider: customUserProvider),
             SignInFutureBuilder(_signInFuture),
           ],
