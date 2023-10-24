877c4d0001271a1587a388a0df3971d735246cb9
Rajarshi Mazumder <raj1156mazumder@gmail.com>
2023-10-24 10:07:51 +0900

    made some changes to fetch courses

--- lib/projectModules/courseManagement/moduleManagement/fetchModules.dart
+++ lib/projectModules/courseManagement/moduleManagement/fetchModules.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:isms/models/course.dart';
 import 'package:isms/models/module.dart';
 
