7f156b6b650e766ae0f5475222ecce2bb7909bb7
Quentin Heath <q.heath@pvp.co.jp>
2023-11-09 13:30:49 +0900

    splot ExamDataMaster in 2 classes (course exams and module exams)

--- lib/projectModules/courseManagement/moduleManagement/examManagement/examDataMaster.dart
+++ lib/projectModules/courseManagement/moduleManagement/examManagement/examDataMaster.dart
@@ -3,25 +3,30 @@ import 'package:isms/models/course.dart';
 import 'package:isms/models/module.dart';
 import 'package:isms/models/newExam.dart';
 import 'package:isms/projectModules/courseManagement/coursesDataMaster.dart';
-import '../coursesProvider.dart';
+import 'package:isms/projectModules/courseManagement/moduleManagement/moduleDataMaster.dart';
+import '../../coursesProvider.dart';
 
-class ExamDataMaster extends CoursesDataMaster {
-  ExamDataMaster({
-    required this.coursesProvider,
-    required this.course,
+class ModuleExamDataMaster extends ModuleDataMaster {
+  ModuleExamDataMaster({
+    required super.coursesProvider,
+    required super.course,
+    required this.module,
   }) : super() {
-    _courseRef = CoursesDataMaster.coursesRef.doc(course.name);
-    _examsRef = _courseRef.collection("exams");
+    DocumentReference moduleRef = modulesRef.doc(module.title);
+    _examsRef = moduleRef.collection("exams");
   }
-  final CoursesProvider coursesProvider;
-  final Course course;
-  late final DocumentReference _courseRef;
+  final Module module;
   late final CollectionReference _examsRef;
 
-  Future<bool> createCourseExam({required NewExam exam}) async {
+  Future<bool> createModuleExam(
+      {required NewExam exam}) async {
     try {
-      int index = course.examsCount + 1;
-
+      int index = 1;
+      try {
+        index = module.exams!.length + 1;
+      } catch (e) {
+        index = 1;
+      }
       exam.index = index;
       Map<String, dynamic> examMap = exam.toMap();
 
@@ -29,12 +34,9 @@ class ExamDataMaster extends CoursesDataMaster {
 
       await _examsRef.doc(exam.title).set(examMap);
 
-      course.addExam(exam);
+      module.addExam(exam);
       coursesProvider.notifyListeners();
-
-      await _courseRef.update({'examsCount': index});
-
-      print("Exam creation successful");
+      print("Module Exam creation successful");
       return true;
     } catch (e) {
       return false;
@@ -46,28 +48,28 @@ class ExamDataMaster extends CoursesDataMaster {
 
     QuerySnapshot examsListSnapshot =
       await _examsRef.orderBy("index").get();
-    if (course.exams == null)
-      course.exams = [];
+    if (module.exams == null)
+      module.exams = [];
     else
-      course.exams!.clear();
+      module.exams!.clear();
     examsListSnapshot.docs.forEach((element) {
       NewExam exam = NewExam.fromMap(element.data() as Map<String, dynamic>);
-      course.addExam(exam);
+      module.addExam(exam);
     });
-    return course.exams!;
+    return module.exams!;
   }
 
   Future<List<NewExam>> get exams async {
-    if (course.exams != null) {
-      print("course exams in cache, no need to fetch them");
-      return course.exams!;
+    if (module.exams != null) {
+      print("module exams in cache, no need to fetch them");
+      return module.exams!;
     } else {
-      print("course exams not in cache, trying to fetch them");
+      print("module exams not in cache, trying to fetch them");
       try {
         return _fetchExams();
       } catch (e) {
-        print("error while fetching course exams: ${e}");
-        course.exams = null;
+        print("error while fetching module exams: ${e}");
+        module.exams = null;
         return [];
       }
     }
