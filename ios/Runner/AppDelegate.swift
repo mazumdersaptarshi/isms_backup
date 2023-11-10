a58ac38b66077e0a1c30a91d214ad30638d7127c
Alexcalyx <a.mason@pvp.co.jp>
2023-11-10 15:15:34 +0900

    trying to add ios links

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,26 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+  ) -> Bool {
+    if userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+       let incomingURL = userActivity.webpageURL {
+      // Send the URL to the Flutter engine
+      self.handleIncomingLink(url: incomingURL)
+      return true
+    }
+    return false
+  }
+
+  private func handleIncomingLink(url: URL) {
+    guard let controller = window?.rootViewController as? FlutterViewController else {
+      return
+    }
+    let channel = FlutterMethodChannel(name: "app/isms/deeplink", binaryMessenger: controller.binaryMessenger)
+    channel.invokeMethod("handleIncomingLink", arguments: url.absoluteString)
+  }
 }
