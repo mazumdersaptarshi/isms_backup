7bb97e040fa7624f3e1e88e854e51a35ed885c9f
sahithay7 <147131723+sahithay7@users.noreply.github.com>
2023-11-14 14:43:43 +0900

    Update examCreationScreen.dart

--- lib/screens/learningModuleScreens/examScreens/examCreationScreen.dart
+++ lib/screens/learningModuleScreens/examScreens/examCreationScreen.dart
@@ -6,8 +6,10 @@ import 'package:isms/models/enums.dart';
 import 'package:isms/models/newExam.dart';
 import 'package:isms/screens/learningModuleScreens/courseScreens/coursesListScreen.dart';
 import 'package:isms/themes/common_theme.dart';
+import 'package:isms/userManagement/loggedInState.dart';
 import 'package:isms/utilityFunctions/generateRandom.dart';
-import 'package:isms/utilityWidgets/questionWidget.dart';
+import 'package:isms/screens/learningModuleScreens/examScreens/sharedWidgets/questionWidget.dart';
+import 'package:isms/utilityFunctions/platformCheck.dart';
 import 'package:provider/provider.dart';
 
 import '../../../models/course.dart';
@@ -57,11 +59,12 @@ class ExamCreationState extends State<ExamCreation> {
     CoursesProvider coursesProvider = Provider.of<CoursesProvider>(context);
     examDataMaster =
         ExamDataMaster(course: widget.course, coursesProvider: coursesProvider);
+    double screenWidth = MediaQuery.of(context).size.width;
+    LoggedInState loggedInState = context.watch<LoggedInState>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create Exam'),
-      ),
-      body: Padding(
+      appBar: PlatformCheck.topNavBarWidget(loggedInState, context: context),
+      bottomNavigationBar:
+      PlatformCheck.bottomNavBarWidget(loggedInState, context: context), body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
@@ -69,7 +72,7 @@ class ExamCreationState extends State<ExamCreation> {
             child: Column(
               children: [
                 Container(
-                  width: 500,
+                  width: screenWidth,
                   decoration: customBoxTheme,
                   child: TextFormField(
                     controller: titleController,
@@ -81,7 +84,7 @@ class ExamCreationState extends State<ExamCreation> {
                   height: 15,
                 ),
                 Container(
-                  width: 500,
+                  width: screenWidth,
                   decoration: customBoxTheme,
                   child: TextFormField(
                     controller: passingMarksController,
