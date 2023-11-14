12b84348a3f94ca30660238d4da9c46df82e3ba1
mazumdersaptarshi <s.mazumder@pvp.co.jp>
2023-11-14 17:58:48 +0900

    fixed a small tiling issue in moduleslistpage

--- lib/screens/learningModuleScreens/courseScreens/moduleScreens/modulesListScreen.dart
+++ lib/screens/learningModuleScreens/courseScreens/moduleScreens/modulesListScreen.dart
@@ -171,15 +171,17 @@ class _CoursePageState extends State<CoursePage> {
                                             SizedBox(
                                               width: 30,
                                             ),
-                                            Text(
-                                              widget.course.name.toString(),
-                                              style: customTheme
-                                                  .textTheme.labelLarge!
-                                                  .copyWith(
-                                                      color: Colors.white,
-                                                      fontSize: 30,
-                                                      fontWeight:
-                                                          FontWeight.bold),
+                                            Expanded(
+                                              child: Text(
+                                                widget.course.name.toString(),
+                                                style: customTheme
+                                                    .textTheme.labelLarge!
+                                                    .copyWith(
+                                                        color: Colors.white,
+                                                        fontSize: 30,
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                              ),
                                             ),
                                           ],
                                         ),
@@ -229,8 +231,6 @@ class _CoursePageState extends State<CoursePage> {
                                               .copyWith(color: Colors.white),
                                         ),
                                         Container(
-                                          margin: EdgeInsets.only(
-                                              left: 20, bottom: 10),
                                           child: Padding(
                                             padding: const EdgeInsets.only(
                                                 top: 20.0),
