59bdd8c72927821a38fc68c9c80f8fdd91c23dd0
SKyotani <s.kyotani@pvp.co.jp>
2024-02-01 15:30:29 +0900

    Separated data for bar chart. Let bar chart to show student name instead of number.

--- lib/views/screens/testing/test_ui_type1/graphs.dart
+++ lib/views/screens/testing/test_ui_type1/graphs.dart
@@ -1,176 +1,10 @@
 import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'bar_chart_data.dart';
 
 class GraphsPage extends StatelessWidget {
-  final Map<String, Map<String, List<Map<String, Map<String, int>>>>> examData =
-  {
-    "course1": {
-      "exam1": [
-        {
-          "student1": {"score": 45},
-          "student2": {"score": 90},
-          "student3": {"score": 75}
-        }
-      ],
-      "exam2": [
-        {
-          "student1": {"score": 73},
-          "student2": {"score": 50},
-          "student3": {"score": 35}
-        }
-      ],
-      "exam3": [
-        {
-          "student1": {"score": 15},
-          "student2": {"score": 90},
-          "student3": {"score": 100}
-        }
-      ]
-    },
-    "course2": {
-      "exam1": [
-        {
-          "student1": {"score": 95},
-          "student2": {"score": 26},
-          "student3": {"score": 54}
-        }
-      ],
-      "exam2": [
-        {
-          "student1": {"score": 72},
-          "student2": {"score": 74},
-          "student3": {"score": 71}
-        }
-      ],
-      "exam3": [
-        {
-          "student1": {"score": 69},
-          "student2": {"score": 65},
-          "student3": {"score": 67}
-        }
-      ]
-    },
-    "course3": {
-      "exam1": [
-        {
-          "student1": {"score": 95},
-          "student2": {"score": 51},
-          "student3": {"score": 30}
-        }
-      ],
-      "exam2": [
-        {
-          "student1": {"score": 100},
-          "student2": {"score": 71},
-          "student3": {"score": 34}
-        }
-      ],
-      "exam3": [
-        {
-          "student1": {"score": 96},
-          "student2": {"score": 98},
-          "student3": {"score": 97}
-        }
-      ]
-    },
-    "course4": {
-      "exam1": [
-        {
-          "student1": {"score": 77},
-          "student2": {"score": 84},
-          "student3": {"score": 80}
-        }
-      ],
-      "exam2": [
-        {
-          "student1": {"score": 22},
-          "student2": {"score": 58},
-          "student3": {"score": 65}
-        }
-      ],
-      "exam3": [
-        {
-          "student1": {"score": 85},
-          "student2": {"score": 58},
-          "student3": {"score": 13}
-        }
-      ]
-    },
-    "course5": {
-      "exam1": [
-        {
-          "student1": {"score": 65},
-          "student2": {"score": 62},
-          "student3": {"score": 70}
-        }
-      ],
-      "exam2": [
-        {
-          "student1": {"score": 68},
-          "student2": {"score": 71},
-          "student3": {"score": 73}
-        }
-      ],
-      "exam3": [
-        {
-          "student1": {"score": 95},
-          "student2": {"score": 67},
-          "student3": {"score": 72}
-        }
-      ]
-    },
-    "course6": {
-      "exam1": [
-        {
-          "student1": {"score": 70},
-          "student2": {"score": 32},
-          "student3": {"score": 88}
-        }
-      ],
-      "exam2": [
-        {
-          "student1": {"score": 85},
-          "student2": {"score": 57},
-          "student3": {"score": 39}
-        }
-      ],
-      "exam3": [
-        {
-          "student1": {"score": 53},
-          "student2": {"score": 91},
-          "student3": {"score": 14}
-        }
-      ]
-    },
-    "course7": {
-      "exam1": [
-        {
-          "student1": {"score": 45},
-          "student2": {"score": 60},
-          "student3": {"score": 98}
-        }
-      ],
-      "exam2": [
-        {
-          "student1": {"score": 52},
-          "student2": {"score": 64},
-          "student3": {"score": 97}
-        }
-      ],
-      "exam3": [
-        {
-          "student1": {"score": 40},
-          "student2": {"score": 32},
-          "student3": {"score": 68}
-        }
-      ]
-    }
-  };
-
-
   Widget build(BuildContext context) {
     List<Widget> courseCharts = _buildCourseCharts();
     return Scaffold(
@@ -180,7 +14,7 @@ class GraphsPage extends StatelessWidget {
       body: SingleChildScrollView(
         // Make the whole page scrollable
         child: Padding(
-          padding: const EdgeInsets.all(100.0),
+          padding: const EdgeInsets.all(40),
           child: Column(
             children: courseCharts,
           ),
@@ -199,9 +33,9 @@ class GraphsPage extends StatelessWidget {
 
       courseCharts.add(
         Card(
-          margin: EdgeInsets.all(8),
+          margin: EdgeInsets.all(16),
           child: Padding(
-            padding: EdgeInsets.all(26),
+            padding: EdgeInsets.all(24),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -276,7 +110,7 @@ class GraphsPage extends StatelessWidget {
                     bottomTitles: AxisTitles(
                       sideTitles: SideTitles(
                         showTitles: true,
-                        getTitlesWidget: _getTitles,
+                        getTitlesWidget: _getStudentNames,
                         reservedSize: 28,
                       ),
                     ),
@@ -325,4 +159,31 @@ class GraphsPage extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget _getStudentNames(double value, TitleMeta meta) {
+  int index = value.toInt();
+  String studentName = '';
+  examData.forEach((course, exams) {
+    exams.forEach((exam, scoresList) {
+      scoresList.forEach((studentScores) {
+        studentScores.forEach((name, data) {
+          if (index == 0) {
+            studentName = name;
+          }
+          index--;
+        });
+      });
+    });
+  });
+
+  return Transform.rotate(
+    angle: -15 * (pi / 180), // -45度回転（ラジアン単位）
+    child: Text(
+      studentName,
+      style: TextStyle(
+        fontSize: 13,
+      ),
+    ),
+  );
 }
